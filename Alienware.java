228b57008534ee153483d5f6d1792ac12d5e5f0f
MallikarjunMD <142385735+MallikarjunMD@users.noreply.github.com>
2023-10-21 12:25:52 +0530

    Add files via upload

---
+++ Alienware.java
@@ -0,0 +1,17 @@
+class Alienware
+{
+	static String dell()
+    {
+		return "Laptop";
+	}
+	static String flipKart()
+	{
+		String x=dell();
+		return x;
+	}
+	public static void main(String[] args) 
+	{
+		String y=flipKart();
+		System.out.println(y);
+	}
+}
